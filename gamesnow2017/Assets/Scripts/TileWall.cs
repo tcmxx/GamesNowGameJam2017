5e2511d99dc6bacd359c9ace03d9086d98dee46c
maxiaoxiao1991 <maxiaoxiao1991@gmail.com>
2017-05-10 17:05:08 +0300

    animation integrated with footsteps

--- gamesnow2017/Assets/Scripts/TileWall.cs
+++ gamesnow2017/Assets/Scripts/TileWall.cs
@@ -7,8 +7,12 @@ public class TileWall : BasicTile {
     
     public override void OnCharacterCollisionEnter(CharacterControl charactor)
     {
-        if(charactor == inTileCharacter || charactor.BasicCharactor.Status == CharactorMovement.CharacterStatus.Jumping)
-            charactor.StartJumping();
+        if (charactor.CurrentStage == AgeStage.Kid
+             || charactor.CurrentStage == AgeStage.Teen)
+        {
+            if (charactor == inTileCharacter || charactor.BasicCharactor.Status == CharactorMovement.CharacterStatus.Jumping)
+                charactor.StartJumping();
+        }
     }
     public override void OnCharacterCollisionExit(CharacterControl charactor)
     {
